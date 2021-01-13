95237132e4bfa3746d3f004ac6e07f8bd8221ce8
sp-evangelos-kapantais <evangelos.kapantais@socialpoint.es>
2021-01-13 12:57:51 +0200

    move tasks to Notion

--- src/App.js
+++ src/App.js
@@ -6,22 +6,6 @@ import Aside from './components/Aside';
 import Popup from './components/Popup';
 import { GlobalContext } from './context/GlobalContext';
 
-// TODO: pipeline: fetch both data types -> store stock info wis DB -> when focused again only fetch day info
-// TODO: !!!IMPORTANT handle etf submission - object properties not handled properly
-// TODO: !!!IMPORTANT prevent duplicates (front/back)
-// TODO: store/show asset type in watchlist/portfolio
-// TODO: store/show amount held in watchlist/portfolio
-// TODO: add option to manually add stocks from other markets
-// TODO: latest update difference
-// TODO: abort controllers
-// TODO: Bug: on new stock, quote fetch is successful, error is thrown, but resolves on reload
-// TODO: update once during the weekend, store the latestUpdate on the quote, and check for the date. if the quote has been updated once during the weekend, refrain from fetching data until Monday
-// TODO: Bug: app is deleting the wrong stock
-// TODO: in StockDetails add a loader until all of the data is fetched
-// TODO: style: responsive
-// TODO: Bug: now I am able to add to the portfolio a stock that already exists in my watchlist.
-// TODO: merge portfolio and watchlist handlers in GlobalContext
-
 function App() {
 	const { getDbStocks, display } = useContext(GlobalContext);
 
